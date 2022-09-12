c1f5ad80198c62600e158a127e7f280ee2664a85
CosmoMax10 <cosmomax10@gmail.com>
2022-09-12 11:08:31 +0900

    finished 80% of the course!!

--- blockchain_server.py
+++ blockchain_server.py
@@ -1,12 +1,13 @@
 
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 import blockchain
 import wallet
 
-
 app = Flask(__name__)
+
 
 cache = {}
 def get_blockchain():
@@ -22,14 +23,61 @@ def get_blockchain():
             'blockchain_address': miners_wallet.blockchain_address})
     return cache['blockchain']
 
+
 @app.route('/chain', methods=['GET'])
 def get_chain():
     block_chain = get_blockchain()
     response = {
-        'chain' : block_chain.chain
+        'chain': block_chain.chain
     }
     return jsonify(response), 200
 
+
+@app.route('/transactions', methods=['GET', 'POST'])
+def transaction():
+    block_chain = get_blockchain()
+    if request.method == 'GET':
+        transactions = block_chain.transaction_pool
+        response = {
+            'transactions': transactions,
+            'length': len(transactions)
+        }
+        return jsonify(response), 200
+
+    if request.method == 'POST':
+        request_json = request.json
+        required = (
+            'sender_blockchain_address',
+            'recipient_blockchain_address',
+            'value',
+            'sender_public_key',
+            'signature')
+        if not all(k in request_json for k in required):
+            return jsonify({'message': 'missing values'}), 400
+
+        is_created = block_chain.create_transaction(
+            request_json['sender_blockchain_address'],
+            request_json['recipient_blockchain_address'],
+            request_json['value'],
+            request_json['sender_public_key'],
+            request_json['signature'],
+        )
+        if not is_created:
+            return jsonify({'message': 'fail'}), 400
+        return jsonify({'message': 'success'}), 201
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    block_chain = get_blockchain()
+    is_mined = block_chain.mining()
+    if is_mined:
+        return jsonify({'message': 'success'}), 200
+    return jsonify({'message': 'fail'}), 400
+
+@app.route('/mine/start', methods=['GET'])
+def start_mine():
+    get_blockchain().start_mining()
+    return jsonify({'message': 'success'}), 200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
@@ -40,5 +88,6 @@ if __name__ == '__main__':
     port = args.port
 
     app.config['port'] = port
+    
 
     app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
